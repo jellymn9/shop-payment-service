ae4bee4bacab5f057eb746f7fc673904e2823ea3
Jelena <jelena.mandzic@gmail.com>
2026-06-24 13:25:27 +0200

    Include cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import cors from "cors";
 import orderRoutes from "./routes/order.routes";
 import paypalRoutes from "./routes/paypal.routes";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use("/paypal", paypalRoutes);
 
