0030bbfc291c303dfb4f9e77f440cb1dcc549d59
Jelena <jelena.mandzic@gmail.com>
2026-06-25 13:46:30 +0200

    refartor: store firebase admin values in env instead json

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
