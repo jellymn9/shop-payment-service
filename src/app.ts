dc6f79ed93a07744ddc1cb008f65824a6689c069
Jelena <jelena.mandzic@gmail.com>
2026-06-27 12:26:33 +0200

    Remove testing route
    Add OPTIONS to cors methods

--- src/app.ts
+++ src/app.ts
@@ -8,36 +8,13 @@ const app = express();
 app.use(
   cors({
     origin: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
-app.use("/paypal", paypalRoutes);
-
 app.use(express.json());
+app.use("/paypal", paypalRoutes);
 app.use("/orders", orderRoutes);
-
-//temporary route for testing
-app.get("/debug-google", async (_req, res) => {
-  try {
-    const response = await fetch(
-      "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com",
-    );
-
-    const text = await response.text();
-
-    res.status(200).json({
-      status: response.status,
-      bodyStart: text.slice(0, 200),
-    });
-  } catch (error) {
-    console.error(error);
-
-    res.status(500).json({
-      error: error instanceof Error ? error.message : "Unknown error",
-    });
-  }
-});
 
 export default app;
